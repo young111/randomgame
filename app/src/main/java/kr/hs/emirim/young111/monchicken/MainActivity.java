eba6efa3f17768abed5a76042b7d6ccd4fd01227
YoungSuk Jo <cyeternal524@naver.com>
2016-04-05 19:05:03 +0900

    #4 add intent code

--- app/src/main/java/kr/hs/emirim/young111/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/young111/monchicken/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.young111.monchicken;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        Toast.makeText(this,"집갈래요..",Toast.LENGTH_LONG).show;
+
+        Toast.makeText(this, "집갈래요..", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }

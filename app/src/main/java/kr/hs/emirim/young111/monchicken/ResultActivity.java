051052cc4d7814d5dacdc3cfb6b8de62f664f181
YoungSuk Jo <cyeternal524@naver.com>
2016-04-07 20:58:09 +0900

    #7 result add

--- app/src/main/java/kr/hs/emirim/young111/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/young111/monchicken/ResultActivity.java
@@ -3,26 +3,67 @@ package kr.hs.emirim.young111.monchicken;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by appcreator29 on 2016. 3. 28..
  */
 public class ResultActivity extends AppCompatActivity {
-
+    static String TAG = "먼치킨 : ResultActivity";
     TextView mResult;
-
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Log.d(TAG, "결과 액티비티 시작!");
+
 
         mResult = (TextView)findViewById(R.id.result);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        Log.d(TAG, "택스트뷰 연결 성공!");
 
+        Random r = new Random();
+        int result = r.nextInt(3);
+        Log.d(TAG, "랜덤값 생성! : "+result);
+
+        mImageView.setImageResource(R.drawable.c01 + result);
+
+        //if
+
+        /*if(result==0){
+            mimageView.setImageResource(R.drawable.c01);
+        }else if(result==1){
+            mimageView.setImageResource(R.drawable.c02);
+        }else if(result==2){
+            mimageView.setImageResource(R.drawable.c03);
+        }*/
+
+        //switch
+        switch(result){
+            case 0:
+                mImageView.setImageResource(R.drawable.ch01); break;
+            case 1:
+                mImageView.setImageResource(R.drawable.ch02); break;
+            case 2:
+                mImageView.setImageResource(R.drawable.ch03); break;
+
+        }
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
+        Log.d(TAG, "인텐트값 읽기<name> : "+name);
         int age = intent.getIntExtra("age", -1);
-        mResult.setText(name+"님 안녕하세요");
+
+        Log.d(TAG, "인텐트값 읽기<age> : " + age);
+        mResult.setText(name + "님 안녕하세요");
     }
 }
+
